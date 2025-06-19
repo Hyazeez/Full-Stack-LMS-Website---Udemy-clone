ba2981d70798b5a5080598e1e041dff1cc97433d
Azeez shaban <hyazeez123@gmail.com>
2025-06-19 20:42:38 +0530

    Create react components

---
+++ src/Context/AppContext.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { AppContext } from "./AppContext";
+
+export const AppContextProvider = (props) => {
+    const value = {
+
+    };
+
+    return (
+        <AppContext.Provider value={value}>
+            {props.children}
+        </AppContext.Provider>
+    );
+};
---
+++ src/Pages/Students/Myenrollmants.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Myenrollmants = () => {
+  return (
+    <div>Myenrollmants</div>
+  )
+}
+
+export default Myenrollmants
